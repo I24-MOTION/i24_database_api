2455cebbb9aca4de45d9f004c13b31f24ae1032f
Yanbing Wang <yanbing.wang@vanderbilt.edu>
2022-11-11 11:08:07 -0600

    add more!

--- src/i24_database_api/transform2.py
+++ src/i24_database_api/transform2.py
@@ -11,18 +11,40 @@ import numpy as np
 import pandas as pd
 import pymongo
 from pymongo import UpdateOne
-from bson.objectid import ObjectId
+import queue
+import time
 
 dt = 0.04
+class LRUCache:
+    """
+    A least-recently-used cache with integer capacity
+    To roll out of the cache for vehicle color and dimensions
+    get(): return the key-value in cache if exists, otherwise return -1
+    put(): (no update) 
+    """
+    def __init__(self, capacity):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+ 
+    def get(self, key, default=None):
         
-def thread_update_one(collection, filter, update, upsert=True):
-    collection.update_one(filter, update, upsert)
-    return
-
-def thread_insert_one(collection, doc):
-    collection.insert_one(doc)
-    return
-   
+        try:
+            item = self.cache[key]
+            self.cache.move_to_end(key)
+            return item
+        except KeyError: # key not in cache
+            self.cache[key] = default 
+            return self.cache[key]
+        
+    def put(self, key, value):
+        
+        self.cache[key] = value
+        
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
+            
+        self.cache.move_to_end(key)   
+    
 
 def decimal_range(start, stop, increment):
     while start < stop: # and not math.isclose(start, stop): Py>3.5
@@ -31,7 +53,7 @@ def decimal_range(start, stop, increment):
         
         
         
-def transform2(direction, config_params, chunk_size=50, write_meta=False):
+def transform_beta(direction, config_params, bulk_write_que, chunk_size=50):
     '''
     direction: eb or wb
     query trajectories that starts in range [start_time, end_time)
@@ -45,12 +67,12 @@ def transform2(direction, config_params, chunk_size=50, write_meta=False):
             "_id":
             "timetamp": t1,
             "eb": {
-                "traj1_id": [centerx, centery, l, w, dir, v, videonode],
+                "traj1_id": [centerx, centery, l, w, dir, v, a, cls, videonode],
                 "traj2_id": [...],
                 ...
                 },
             "wb": {
-                "traj3_id": [centerx, centery, l, w, dir, v, videonode],
+                "traj3_id": [centerx, centery, l, w, dir, v, a, cls, videonode],
                 "traj4_id": [...],
                 ...
                 },
@@ -94,48 +116,14 @@ def transform2(direction, config_params, chunk_size=50, write_meta=False):
 
 
     from_collection = client[config_params['read_database_name']][config_params['read_collection_name']]
-    to_collection = client[config_params["write_database_name"]][config_params["write_collection_name"]]
     
-    # add schema to the meta collection
-    if write_meta:
-        meta_col = client[config_params["write_database_name"]]["__METADATA__"]
-        start_time = from_collection.find_one(sort=[("first_timestamp", 1)])["first_timestamp"]
-        end_time = from_collection.find_one(sort=[("last_timestamp", -1)])["last_timestamp"]
-        start_x = from_collection.find_one(sort=[("starting_x", 1)])["starting_x"]
-        end_x = from_collection.find_one(sort=[("ending_x", -1)])["ending_x"]
-        
-        meta_doc = {
-            "_id": config_params['read_collection_name'],
-            "name": "",
-            "description": "",
-            "start_time": start_time,
-            "end_time": end_time,
-            "num_objects": from_collection.estimated_document_count(),
-            "duration": end_time-start_time,
-            "start_x": start_x,
-            "end_x": end_x, 
-            "road_segment_length": abs(start_x-end_x)
-            }
-        try:
-            meta_col.insert_one(meta_doc, bypass_document_validation=True)
-        except:
-            _id = config_params['read_collection_name']
-            meta_doc.pop("_id")
-            meta_col.update_one({"_id": _id}, {"$set": meta_doc}, upsert=True)
-            
-            
-        print("Collection information is written to __METADATA__")
-    
-    
-    
-    from_collection.create_index("compute_node_id")
-    to_collection.create_index("timestamp")
     time_series_field = ["timestamp", "x_position", "y_position", "length", "width"]
       
     lru = OrderedDict()
+    attr_lru = LRUCache(1000)
     stale = defaultdict(int) # key: timestamp, val: number of timestamps that it hasn't been updated
-    stale_thresh = 1000 # if a timestamp is not updated after processing [stale_thresh] number of trajs, then update to database. stale_thresh~=#veh on roadway simulataneously
-    last_poped_t = 0
+    stale_thresh = 500 # if a timestamp is not updated after processing [stale_thresh] number of trajs, then update to database. stale_thresh~=#veh on roadway simulataneously
+    # last_poped_t = 0
     
     dir = 1 if direction=="eb" else -1
     start = from_collection.find_one(sort=[("first_timestamp", 1)])["first_timestamp"]
@@ -145,16 +133,13 @@ def transform2(direction, config_params, chunk_size=50, write_meta=False):
       
     # specify query - iterative ranges
     for s in decimal_range(start, end, chunk_size):
-        print("Query range: {:.2f}-{:.2f}".format(s, s+chunk_size))
+        print("{} In progress (approx) {:.1f} %".format(direction, (s+chunk_size-start)/(end-start)*100))
         all_trajs = from_collection.find({"direction":dir, "first_timestamp": {"$gte": s, "$lt": s+chunk_size}}).sort("first_timestamp",1)
-        
-        bulk_write_cmd = []
-        cache = {}
         
         for traj in all_trajs:
             
-            _id, l,w,node = traj["_id"], traj["length"], traj["width"], traj["compute_node_id"]
-            cache[_id] = node
+            _id, l,w,node, ccls = traj["_id"], traj["length"], traj["width"], traj["compute_node_id"], traj["coarse_vehicle_class"] 
+            attr_lru.put(_id, [ccls,node])
             
             if isinstance(l, float):
                 n = len(traj["x_position"])
@@ -166,14 +151,19 @@ def transform2(direction, config_params, chunk_size=50, write_meta=False):
                 velocity = list(dir*np.diff(traj["x_position"])/dt)
                 velocity.append(velocity[-1])
         
-            # increment stale
-            for k in stale:  
-                stale[k] += 1
+            # finite difference twice to get acceleration
+            accel = list(np.diff(traj["x_position"], n=2)/(dt**2))
+            last_accel = accel[-1]
+            accel.extend([last_accel,last_accel]) 
             
+            # increment stale
+            for k in stale:
+                stale[k] += 1
             
             # resample to 1/dt hz
             data = {key:traj[key] for key in time_series_field}
             data["velocity"] = velocity
+            data["acceleration"] = accel
             data["length"] = l
             data["width"] = w
             df = pd.DataFrame(data, columns=data.keys()) 
@@ -192,10 +182,7 @@ def transform2(direction, config_params, chunk_size=50, write_meta=False):
             first_time = max(min(traj['timestamp']), first_valid_time-1)
             last_time = min(max(traj['timestamp']), last_valid_time+1)
             df=df[first_time:last_time]
-            
-            # traj['x_position'] = list(df['x_position'].values)
-            # traj['y_position'] = list(df['y_position'].values)
-            # traj['timestamp'] = list(df.index.values)
+
             traj["first_timestamp"] = df.index.values[0]
             traj["last_timestamp"] = df.index.values[-1]
             traj["starting_x"] = df['x_position'].values[0]
@@ -205,18 +192,22 @@ def transform2(direction, config_params, chunk_size=50, write_meta=False):
             # add to result dictionary
             for t in df.index:
                 # [centerx, centery, l ,w, dir, v]
+                ccls, node = attr_lru.get(_id)
                 try:
+                    
                     lru[t][str(_id)] = [df["x_position"][t] + dir*0.5*df["length"][t],
                                         df["y_position"][t],
                                         df["length"][t],
                                         df["width"][t], 
                                         dir,
                                         df["velocity"][t],
-                                        cache[_id]]
+                                        df["acceleration"][t],
+                                        ccls,
+                                        node] # dir, v, a, cls, videonode],
                 except: # t does not exists in lru yet
-                    if t <= last_poped_t:
-                        # meaning t was poped pre-maturely
-                        print("t was poped prematurely from LRU in transform_queue. Increase stale")
+                    # if t <= last_poped_t:
+                    #     # meaning t was poped pre-maturely
+                    #     print("t was poped prematurely from LRU in transform_queue. Increase stale")
                         
                     lru[t] = {str(_id): [df["x_position"][t] + dir*0.5*df["length"][t],
                                         df["y_position"][t],
@@ -224,27 +215,22 @@ def transform2(direction, config_params, chunk_size=50, write_meta=False):
                                         df["width"][t], 
                                         dir,
                                         df["velocity"][t],
-                                        cache[_id]]}
+                                        df["acceleration"][t],
+                                        ccls,
+                                        node] }
                 lru.move_to_end(t, last=True)
                 stale[t] = 0 # reset staleness
                 
             # update db from lru
             while stale[next(iter(lru))] > stale_thresh:
                 t, d = lru.popitem(last=False) # pop first
-                last_poped_t = t
+                # last_poped_t = t
                 stale.pop(t)
                 # change d to value.objectid: array, so that it does not reset the value field, but only update it
                 query = {"timestamp": round(t,2)}
                 update = {"$set": {direction+"."+key: val for key,val in d.items()}}
-                bulk_write_cmd.append(UpdateOne(filter=query, update=update, upsert=True))
-                
-                # pool.apply_async(thread_update_one, (to_collection, {"timestamp": round(t,2)},{"$set": d},))
+                bulk_write_que.put(UpdateOne(filter=query, update=update, upsert=True))
             
-            # bulk write all the complete documents to db
-            if len(bulk_write_cmd) > 500:
-                to_collection.bulk_write(bulk_write_cmd, ordered=False)
-                bulk_write_cmd = []
-                
                 
         # write the rest of lru to database
         print("Flush out the rest in LRU cache")
@@ -254,15 +240,84 @@ def transform2(direction, config_params, chunk_size=50, write_meta=False):
             # pool.apply_async(thread_update_one, (to_collection, {"timestamp": round(t,2)},{"$set": d},))
             query = {"timestamp": round(t,2)}
             update = {"$set": {direction+"."+key: val for key,val in d.items()}}
-            bulk_write_cmd.append(UpdateOne(filter=query, update=update, upsert=True))
-            
-        if len(bulk_write_cmd) > 0:
-            to_collection.bulk_write(bulk_write_cmd, ordered=False)
+            bulk_write_que.put(UpdateOne(filter=query, update=update, upsert=True))
             
 
-    del to_collection
     del from_collection
     return 
+
+
+
+
+def batch_write(config_params, bulk_write_queue):
+    
+    time.sleep(10)
+    client_host=config_params['host']
+    client_username=config_params['username']
+    client_password=config_params['password']
+    client_port=config_params['port']
+
+    client=pymongo.MongoClient(host=client_host,
+        port=client_port,
+        username=client_username,
+        password=client_password,
+        connect=True,
+        connectTimeoutMS=5000)
+
+
+    from_collection = client[config_params['read_database_name']][config_params['read_collection_name']]
+    to_collection = client[config_params["write_database_name"]][config_params["write_collection_name"]]
+    to_collection.create_index("timestamp")
+    
+    # add schema to the meta collection
+    meta_col = client[config_params["write_database_name"]]["__METADATA__"]
+    start_time = from_collection.find_one(sort=[("first_timestamp", 1)])["first_timestamp"]
+    end_time = from_collection.find_one(sort=[("last_timestamp", -1)])["last_timestamp"]
+    start_x = from_collection.find_one(sort=[("starting_x", 1)])["starting_x"]
+    end_x = from_collection.find_one(sort=[("ending_x", -1)])["ending_x"]
+    
+    meta_doc = {
+        "_id": config_params['read_collection_name'],
+        "name": "",
+        "description": "",
+        "start_time": start_time,
+        "end_time": end_time,
+        "num_objects": from_collection.estimated_document_count(),
+        "duration": end_time-start_time,
+        "start_x": start_x,
+        "end_x": end_x, 
+        "road_segment_length": abs(start_x-end_x)
+        }
+    try:
+        meta_col.insert_one(meta_doc, bypass_document_validation=True)
+    except:
+        _id = config_params['read_collection_name']
+        meta_doc.pop("_id")
+        meta_col.update_one({"_id": _id}, {"$set": meta_doc}, upsert=True)
+        
+        
+    print("Collection information is written to __METADATA__")
+
+    bulk_write_cmd = []
+    
+    while True:
+        try:
+            cmd = bulk_write_queue.get(timeout = 10)
+            bulk_write_cmd.append(cmd)
+            
+        except queue.Empty:
+            print("Getting from bulk_write_queue reaches timeout.")
+            break
+        
+        if len(bulk_write_cmd) > 500:
+            to_collection.bulk_write(bulk_write_cmd, ordered=False)
+            bulk_write_cmd = []
+        
+        
+    if len(bulk_write_cmd) > 0:
+        to_collection.bulk_write(bulk_write_cmd, ordered=False)
+    
+    return
 
 
 
@@ -281,7 +336,7 @@ if __name__ == '__main__':
     db_param["write_database_name"] = "transformed_beta"
     db_param["write_collection_name"] = db_param["read_collection_name"]
     
-    transform2(chunk_size=20)
+    transform_beta(chunk_size=20)
     
     # print("not implemented")
     
